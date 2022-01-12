afc396d2ad32dd91fe6a039df1f3ee5645e58911
promehedi <mmhasan563@gmail.com>
2022-01-12 17:50:26 +0600

    Create DStorage Contract

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -2,23 +2,32 @@
 pragma solidity ^0.8.0;
 
 contract DStorage {
-  // Name
-  // Number of files
-  // Mapping fileId=>Struct 
+  // Variables
+  string public name = "DStorage";
+  uint public fileCount = 0;
+  mapping(uint => File) public files;
 
   // Struct
-
+  struct File {
+    uint id;
+    string hash;
+    uint size;
+    string fileType;
+    string name;
+    string description;
+    uint timestamp;
+    address owner;
+  }
 
   // Event
 
-  constructor() public {
+  constructor() {
   }
 
-  // Upload File function
+  function uploadFile(string memory _hash, uint _size, string memory _fileType, string memory _name, string memory _description) public {
 
-    // Make sure the file hash exists
-
-    // Make sure file type exists
+    fileCount++;
+    files[fileCount] = File(fileCount, _hash, _size, _fileType, _name, _description, block.timestamp, msg.sender);
 
     // Make sure file description exists
 
@@ -34,5 +43,6 @@ contract DStorage {
     // Add File to the contract
 
     // Trigger an event
+  }
 
 }
