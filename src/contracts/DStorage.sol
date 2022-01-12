80bce87b4672d4c1f45d8126a6d2fe79f5377c59
promehedi <mmhasan563@gmail.com>
2022-01-12 17:55:00 +0600

    Add FileUploaded Event

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -20,9 +20,7 @@ contract DStorage {
   }
 
   // Event
-
-  constructor() {
-  }
+  event FileUploaded(uint fileId, string hash, uint size, string fileType, string name, string description, uint timestamp, address owner);
 
   function uploadFile(string memory _hash, uint _size, string memory _fileType, string memory _name, string memory _description) public {
     require(bytes(_hash).length == 32);
@@ -35,20 +33,6 @@ contract DStorage {
     fileCount++;
     files[fileCount] = File(fileCount, _hash, _size, _fileType, _name, _description, block.timestamp, msg.sender);
 
-    // Make sure file description exists
-
-    // Make sure file fileName exists
-
-    // Make sure uploader address exists
-
-    // Make sure file size is more than 0
-
-
-    // Increment file id
-
-    // Add File to the contract
-
-    // Trigger an event
+    emit FileUploaded(fileCount, _hash, _size, _fileType, _name, _description, block.timestamp, msg.sender);
   }
-
 }
